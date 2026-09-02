584aa9a9a67e5d53bbbd5c941a3367ec396b4240
Arsham <arshamhaqiqat@gmail.com>
2026-09-03 00:05:00 +0330

    Parallel mining made idiomic

--- paralell mining/main.go
+++ paralell mining/main.go
@@ -3,123 +3,100 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"math"
 	"math/rand"
 	"sync"
-	"time"
 )
 
-// This is a very good example of go routines and why we need waitgroups
-// if we don't use waitgroups "the miner %v done!" won't print.
-// when we say defer wg.done() this line will execute after the function is exited.
-
-//don't put wg.Add() inside go func. If you call wg.Add(1) inside the goroutine, there’s a race condition:
-//The main goroutine might call wg.Wait() before your Add happens
-// Here in this code it won't happen even though I put ad inside the start_mining bc of the logic.
-
-//you can also use wg.go() which is the short term of the add and done: less understandable but also less coding
-
-// func (wg *WaitGroup) Go(f func()) {
-//     wg.Add(1)
-//     go func() {
-//         defer wg.Done()
-//         f()
-//     }()
-// }
-
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-func randomString(n int) string {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
-
 type Block struct {
-	Data  string
-	Nonce int
-	Hash  [32]byte
+	ID   int
+	Data string
 }
 
-type Results struct {
-	b          Block
-	miner_name string
+type Result struct {
+	MinerID int
+	BlockID int
+	Nonce   int
+	Hash    string
 }
 
-func main() {
-	c := make(chan Block)
-	c2 := make(chan Results)
-
-	miners_list := []string{"m1", "m2", "m3"}
-
-	for range 5 {
-		var new_block Block
-		new_block.Data = randomString(7)
-		go func(b Block) {
-			c <- b
-		}(new_block)
-	}
-
+func mineBlock(block Block, minerCount int, difficulty int, score map[int]int) Result {
+	done := make(chan struct{})    // closed the instant someone wins
+	winner := make(chan Result, 1) // buffered so the winning send never blocks
+	var once sync.Once
 	var wg sync.WaitGroup
-	go miners_start_working(c, c2, miners_list, &wg)
 
-	b_counter := 0
-	var full_res [5]Results
+	for m := 0; m < minerCount; m++ {
+		wg.Add(1)
+		go func(minerID int) {
+			defer wg.Done()
 
-	for b_counter <= 4 {
-		full_res[b_counter] = <-c2
-		fmt.Printf("Mined Block: %+v", full_res[b_counter].b)
-		fmt.Printf("\n Miners's name: %v", full_res[b_counter].miner_name)
-		fmt.Println("\n---------------------------------")
-		b_counter++
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					nonce := rand.Intn(math.MaxInt32)
+					hash := sha256.Sum256([]byte(block.Data + fmt.Sprint(nonce)))
+					if hasLeadingZeroBits(hash[:], difficulty) {
+						once.Do(func() {
+							winner <- Result{MinerID: m, BlockID: block.ID, Nonce: nonce, Hash: fmt.Sprint(hash)}
+							close(done)
+						})
+					}
+				}
+			}
+		}(m)
 	}
 
-	close(c)
-	wg.Wait()
-	close(c2)
-	fmt.Println("\n--------------------------------- \n All blocks done")
+	wg.Wait() // block until every miner has stopped (won, or bailed via done)
+	result := <-winner
+	score[result.MinerID]++
+	return result
 }
 
-func mine(c chan Block, c2 chan Results, name string) bool {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	var b Block
-	b, ok := <-c
-	if !ok {
-		return true
+func hasLeadingZeroBits(hash []byte, n int) bool {
+	if n < 0 || n > len(hash)*8 {
+		return false
 	}
-	for {
-		b.Nonce = r.Int()
 
-		data := fmt.Sprintf("%s%d", b.Data, b.Nonce)
-		hash := sha256.Sum256([]byte(data))
+	fullBytes := n / 8
+	remainingBits := n % 8
 
-		// Difficulty: first 6 hex characters must be "000000"
-		// That means first 24 bits must be zero.
-		if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
-			b.Hash = hash
-			var res Results
-			res.b = b
-			res.miner_name = name
-			c2 <- res
+	for i := 0; i < fullBytes; i++ {
+		if hash[i] != 0 {
 			return false
 		}
 	}
+	if remainingBits > 0 {
+		mask := byte(0xFF << (8 - remainingBits))
+		if hash[fullBytes]&mask != 0 {
+			return false
+		}
+	}
+	return true
 }
 
-func miners_start_working(c chan Block, c2 chan Results, miners_lst []string, wg *sync.WaitGroup) {
-	for i := range 3 {
-		wg.Add(1) // could've also used wg.go func ...
-		go func(i int) {
-			// defer wg.done() ---> this is safer bc if we have panic or sth this still executes.
-			for {
-				not_ok := mine(c, c2, miners_lst[i])
-				if not_ok {
-					fmt.Printf("\nminer %v Done!", miners_lst[i])
-					wg.Done()
-					break
-				}
-			}
-		}(i)
+func main() {
+	const numMiners = 5
+	const difficulty = 22 // tune so each block takes a moment but not forever
+	blocks := []Block{
+		{ID: 0, Data: "block-0"},
+		{ID: 1, Data: "block-1"},
+		{ID: 2, Data: "block-2"},
+		{ID: 3, Data: "block-3"},
+		{ID: 4, Data: "block-4"},
 	}
+
+	score := make(map[int]int)
+	var ledger []Result
+
+	for _, b := range blocks {
+		result := mineBlock(b, numMiners, difficulty, score)
+		ledger = append(ledger, result)
+		fmt.Printf("block %d won by miner %d (nonce=%d)\n", result.BlockID, result.MinerID, result.Nonce)
+	}
+
+	fmt.Println("final scores:", score)
+	fmt.Println("blocks mined:", len(ledger))
 }
